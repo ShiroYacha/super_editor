8dfdff59cdcbe88f549ecbb94988527c4dd437cb
ShiroYacha <sebzhaomin@gmail.com>
2025-10-18 23:16:32 +0200

    Allow copying markdown when cutting

--- super_editor_clipboard/lib/src/super_editor_copy.dart
+++ super_editor_clipboard/lib/src/super_editor_copy.dart
@@ -12,7 +12,8 @@ ExecutionInstruction copyAsRichTextWhenCmdCOrCtrlCIsPressed({
     return ExecutionInstruction.continueExecution;
   }
 
-  if (!keyEvent.isPrimaryShortcutKeyPressed || keyEvent.logicalKey != LogicalKeyboardKey.keyC) {
+  if (!keyEvent.isPrimaryShortcutKeyPressed ||
+      keyEvent.logicalKey != LogicalKeyboardKey.keyC) {
     return ExecutionInstruction.continueExecution;
   }
   if (editContext.composer.selection == null) {
@@ -26,6 +27,39 @@ ExecutionInstruction copyAsRichTextWhenCmdCOrCtrlCIsPressed({
   editContext.document.copyAsRichTextWithPlainTextFallback(
     selection: editContext.composer.selection!,
   );
+
+  return ExecutionInstruction.haltExecution;
+}
+
+/// [SuperEditor] shortcut to cut the document as rich text when
+/// `CMD + X` (Mac) or `CTRL + X` (Windows/Linux) is pressed.
+ExecutionInstruction cutAsRichTextWhenCmdXOrCtrlXIsPressed({
+  required SuperEditorContext editContext,
+  required KeyEvent keyEvent,
+}) {
+  if (keyEvent is! KeyDownEvent && keyEvent is! KeyRepeatEvent) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  if (!keyEvent.isPrimaryShortcutKeyPressed ||
+      keyEvent.logicalKey != LogicalKeyboardKey.keyX) {
+    return ExecutionInstruction.continueExecution;
+  }
+  if (editContext.composer.selection == null) {
+    return ExecutionInstruction.continueExecution;
+  }
+  if (editContext.composer.selection!.isCollapsed) {
+    // Nothing to cut, but we technically handled the task.
+    return ExecutionInstruction.haltExecution;
+  }
+
+  // Copy the selection as rich text to the clipboard
+  editContext.document.copyAsRichTextWithPlainTextFallback(
+    selection: editContext.composer.selection!,
+  );
+
+  // Delete the selected content
+  editContext.commonOps.deleteSelection(TextAffinity.downstream);
 
   return ExecutionInstruction.haltExecution;
 }
--- super_editor_clipboard/lib/src/super_editor_copy_markdown.dart
+++ super_editor_clipboard/lib/src/super_editor_copy_markdown.dart
@@ -30,3 +30,36 @@ ExecutionInstruction copyAsMarkdownWhenCmdCOrCtrlCIsPressed({
 
   return ExecutionInstruction.haltExecution;
 }
+
+/// [SuperEditor] shortcut to cut the document as markdown when
+/// `CMD + X` (Mac) or `CTRL + X` (Windows/Linux) is pressed.
+ExecutionInstruction cutAsMarkdownWhenCmdXOrCtrlXIsPressed({
+  required SuperEditorContext editContext,
+  required KeyEvent keyEvent,
+}) {
+  if (keyEvent is! KeyDownEvent && keyEvent is! KeyRepeatEvent) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  if (!keyEvent.isPrimaryShortcutKeyPressed ||
+      keyEvent.logicalKey != LogicalKeyboardKey.keyX) {
+    return ExecutionInstruction.continueExecution;
+  }
+  if (editContext.composer.selection == null) {
+    return ExecutionInstruction.continueExecution;
+  }
+  if (editContext.composer.selection!.isCollapsed) {
+    // Nothing to cut, but we technically handled the task.
+    return ExecutionInstruction.haltExecution;
+  }
+
+  // Copy the selection as markdown to the clipboard
+  editContext.document.copyAsMarkdown(
+    selection: editContext.composer.selection!,
+  );
+
+  // Delete the selected content
+  editContext.commonOps.deleteSelection(TextAffinity.downstream);
+
+  return ExecutionInstruction.haltExecution;
+}
